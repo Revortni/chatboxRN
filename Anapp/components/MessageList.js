73cdb0904d3ffe057543e3b4e89464897954a87d
revortni <rojit.blaze13@gmail.com>
2019-06-30 17:11:45 +0545

    added timestamp container and timestamp to messages

--- Anapp/components/MessageList.js
+++ Anapp/components/MessageList.js
@@ -1,6 +1,7 @@
-    import React, {Component} from 'react';
-import { StyleSheet,ScrollView,KeyboardAvoidingView,Keyboard} from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet,ScrollView,KeyboardAvoidingView,Keyboard, View, Text} from 'react-native';
 import Message from './Message';
+import moment from "moment";
 
 export default class MessageList extends Component {
     constructor(props){
@@ -32,8 +33,26 @@ export default class MessageList extends Component {
         return null;
     }
 
+    getTimeStamp = (x,y)=>{
+        last = moment(x);
+        now = moment(y);
+        diff = now.diff(last,"minutes");
+        // alert(`${x} ${y} ${diff}`);
+        if (diff > 5){ 
+            let hdiff = moment().diff(now,"days");
+            if(hdiff>3){
+                return now.format("D MMM [AT] h:mm A");
+            }else if (hdiff>0){
+                return now.format("dddd [AT] h:mm A");
+            }
+            return now.format("h:mm A");
+        }
+        return null;
+    }
+
     render() {
         var lastsender = "";
+        var last = 0;
         return (
             <ScrollView 
                     keyboardShouldPersistTaps="handled"
@@ -44,11 +63,32 @@ export default class MessageList extends Component {
                 }>
                     <KeyboardAvoidingView style={styles.messageContainer}>
                         {this.state.messages.map((x,i)=>{
+                            let timestamp = null;
+                            let content = null;
+                            if(x.action =="sent" || x.action == "rec"){
+                                // alert(`${x.createdAt}`);
+                                timestamp = this.getTimeStamp(last,x.createdAt);
+                                last = x.createdAt;
+                                if(timestamp){
+                                    content = <View style = {styles.timestampContainer}><Text style = {styles.timestamp}>{timestamp}</Text></View>;
+                                }
+                            }
                             if(x.username!=lastsender){
-                                lastsender=x.username
-                                return(<Message type={x.action} content={x.message} username={x.username} key={i} italic={x.italic}/>);
+                                lastsender=x.username;
+                                return(
+                                    <View style = {styles.msgwrapper} key={i}>
+                                        {content}
+                                        <Message type={x.action} content={x.message} username={x.username} key={i}/>
+                                    </View>
+                                
+                                );
                             }                            
-                            return(<Message type={x.action} content={x.message} key={i}/>)
+                            return(
+                                <View style = {styles.msgwrapper} key={i}>
+                                    {content}
+                                    <Message type={x.action} content={x.message} key={i}/>
+                                </View>
+                            )
                         })}
                     </KeyboardAvoidingView>
             </ScrollView>
@@ -60,5 +100,19 @@ export default class MessageList extends Component {
         messageContainer:{
             marginTop:5,
             marginBottom:10,
+        },
+        timestampContainer:{
+            padding:10,
+            alignSelf:'center',
+            flexDirection:'column',
+            marginBottom:2
+        },
+        timestamp:{
+            color:"#696969",
+            fontSize:11,
+            fontFamily:'Roboto'
+        },
+        msgwrapper:{
+
         }
     });
