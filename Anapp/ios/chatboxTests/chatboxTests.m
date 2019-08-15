9b2098e9556fffa94953637b9e81b07d2560d217
revortni <rojit.blaze13@gmail.com>
2019-08-15 14:26:38 +0545

    Updated to react-native 0.60.4 along with other dependency updates

--- Anapp/ios/chatboxTests/chatboxTests.m
+++ Anapp/ios/chatboxTests/chatboxTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface AnappTests : XCTestCase
+@interface chatboxTests : XCTestCase
 
 @end
 
-@implementation AnappTests
+@implementation chatboxTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
