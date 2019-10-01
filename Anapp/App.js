ff3f12a32e1b31d1837c6f35326a06ca2fa2f93d
revortni <rojit.blaze13@gmail.com>
2019-10-01 16:21:59 +0545

    package updates

--- Anapp/App.js
+++ Anapp/App.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import HomeScreen from './components/HomeScreen';
+import {Platform, StyleSheet, Text, View, StatusBar} from 'react-native';
+import HomeScreen from './app/pages/HomeScreen';
 
 export default class App extends Component {
   render() {
     return (
-      <HomeScreen/>
+      <React.Fragment>
+        <View style={{marginBottom:20}}>
+          <StatusBar 
+          translucent 
+          backgroundColor={styles.statusBar.backgroundColor}
+          barStyle="light-content"
+          hidden={false}
+          />
+        </View>
+        <HomeScreen/>
+      </React.Fragment>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  
+  statusBar:{
+    backgroundColor:"#000"
+  }
 });
