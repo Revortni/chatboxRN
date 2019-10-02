7db437f12b186de892304ab7d2639db0579f843a
revortni <rojit.blaze13@gmail.com>
2019-10-02 19:08:06 +0545

    config files

---
+++ Anapp/eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  env: {
+    jest: true
+  },
+  rules: {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'linebreak-style': [2, 'windows']
+  },
+  globals: {
+    fetch: false
+  },
+  plugins: ['react']
+};
