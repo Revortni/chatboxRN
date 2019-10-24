31d8171750ada82ee54feb1c1583f3c09aada98f
revortni <rojit.blaze13@gmail.com>
2019-10-24 18:38:00 +0545

    removed reactnavigation stacknavigator(placed settings inside chatbox component

--- Anapp/App.js
+++ Anapp/App.js
@@ -2,31 +2,13 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Main from './app/pages/Main';
-import Settings from './app/pages/Settings';
  
-const App = createStackNavigator({
-    HomeScreen: {
-        screen: Main,
-        navigationOptions:{
-            
-        }
-    },
-    Settings:{
-        screen: Settings,
-        navigationOptions: {
-            title: 'Settings',
-            headerShown:true        
-        },
-    }
-    },{
-        initialRouteName: 'HomeScreen',
-        defaultNavigationOptions:{
-            headerShown:false,
-            headerForceInset:true,
-            headerMode:'none'
-        }
-});
+const App = ()=>{
+    return (
+        <Main />
+    );
+};
 
 
 
-export default createAppContainer(App);
+export default App;
